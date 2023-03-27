3dc96e211cb707c199fc9071100184f1bdeaecad
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-27 16:41:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Dropdown } from 'react-bootstrap';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import { Jumbotron } from 'react-bootstrap';
 
 
 function App() {
