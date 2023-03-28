1e9e0a6738b8eac62c398bf7038e50492ed8b6bd
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 13:07:07 +0200

    imports fixes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header/Header';
-import CourseTopics from './components/CourseTopics/CourseTopics';
-import Section1 from './components/Sections/Section1/Section1';
-// Import other section components
+import Section1 from './components/Section1/Section1';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="/">Learn Web3</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <CourseTopics />
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
+          <Navbar.Brand href="#home">Learn Web3</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/section1">1. Introduction to Web3 and Cryptocurrencies</Nav.Link>
+              <Nav.Link href="/section2">2. Blockchain Technology Basics</Nav.Link>
+              <Nav.Link href="/section3">3. Ethereum and Smart Contracts</Nav.Link>
+              <Nav.Link href="/section4">4. Decentralized Finance (DeFi)</Nav.Link>
+              <Nav.Link href="/section5">5. Non-fungible Tokens (NFTs)</Nav.Link>
+              <Nav.Link href="/section6">6. Building Decentralized Applications (dApps)</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Switch>
-          <Route exact path="/">
-            <Header />
-          </Route>
-          <Route path="/section1">
-            <Section1 />
-          </Route>
-          {/* Add routes for other sections */}
+          <Route path="/section1" component={Section1} />
+          {/* Add other routes here */}
+          <Redirect from="/" to="/section1" />
         </Switch>
+        <Header />
       </div>
     </Router>
   );
