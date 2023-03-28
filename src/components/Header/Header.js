4a3682f9f3ab019007de8bdc38531c89866312fc
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 12:51:50 +0200

    git commit -m "update"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './Header.css';
 
+function Header() {
+  return (
+    <header className="hero-section">
+      <h1>Welcome to Learn Web3</h1>
+      <p>
+        Explore the world of cryptocurrencies, blockchain, and decentralized applications.
+        Learn about Ethereum, DeFi, NFTs, and more. Start your journey to becoming a Web3 expert today!
+      </p>
+      <img src="./hero-image.jpg" alt="Web3 illustration" />
+    </header>
+  );
+}
+
+export default Header;
