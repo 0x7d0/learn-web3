ef1a0f5e5dd39a81f03e20ae5e8f4126db0839a8
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 12:46:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './Header';
-import Section1 from './Section1';
+import Header from './components/Header/Header';
+import CourseTopics from './components/CourseTopics/CourseTopics';
+import Section1 from './components/Sections/Section1/Section1';
+// Import other section components
 
 function App() {
   return (
-    <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Learn Web3</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="Course Topics" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#section1">1. Introduction to Web3 and Cryptocurrencies</NavDropdown.Item>
-                <NavDropdown.Item href="#section2">2. Blockchain Technology Basics</NavDropdown.Item>
-                <NavDropdown.Item href="#section3">3. Ethereum and Smart Contracts</NavDropdown.Item>
-                <NavDropdown.Item href="#section4">4. Decentralized Finance (DeFi)</NavDropdown.Item>
-                <NavDropdown.Item href="#section5">5. Non-fungible Tokens (NFTs)</NavDropdown.Item>
-                <NavDropdown.Item href="#section6">6. Building Decentralized Applications (dApps)</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Header />
-      <Section1 />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Learn Web3</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <CourseTopics />
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+          </Route>
+          <Route path="/section1">
+            <Section1 />
+          </Route>
+          {/* Add routes for other sections */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
