b75346bff54ca88c2d8467f732a0c71813aed12f
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 14:15:29 +0200

    home update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <Container>
-        <Row>
-          <Col>
-            <h1>Welcome to Learn Web3</h1>
-            <p>
-              Discover the world of web3 technologies and cryptocurrencies. Learn
-              about blockchain, Ethereum, smart contracts, decentralized finance,
-              non-fungible tokens, and more.
-            </p>
-          </Col>
-        </Row>
-      </Container>
+    <div className="home-content">
+      <h1>Master Web3 and Crypto with Our Comprehensive Course</h1>
+      <p>
+        Join the revolution and become an expert in blockchain, cryptocurrencies, and Web3 technologies with our in-depth course. For just $4.99, you'll have access to a wealth of knowledge that will help you unlock the future of finance and the internet.
+      </p>
+      <p>
+        This course is tailored for technically inclined individuals who want to dive deeper into the world of Web3 and cryptocurrencies. Whether you are a developer, entrepreneur, or just curious about the possibilities of decentralization, this course will provide you with a strong foundation in Web3 technologies and the crypto space.
+      </p>
+      <p>
+        With our course, you will:
+        <ul>
+          <li>Learn the fundamentals of blockchain technology and how it's revolutionizing industries</li>
+          <li>Understand Ethereum, smart contracts, and decentralized applications (dApps)</li>
+          <li>Explore the growing world of decentralized finance (DeFi) and non-fungible tokens (NFTs)</li>
+          <li>Acquire practical skills in building your own dApps and working with Web3 libraries</li>
+          <li>Stay up-to-date with the latest trends and developments in the crypto ecosystem</li>
+        </ul>
+      </p>
+      <p>
+        Don't miss this opportunity to invest in your future and be part of the decentralized revolution. Enroll now for just $4.99 and start learning today!
+      </p>
     </div>
   );
-};
+}
 
 export default Home;
