c4fa54c14558bfeb71a85d6152ac06a8e91e22af
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 12:47:21 +0200

    Update CourseTopics.js

--- src/components/CourseTopics/CourseTopics.js
+++ src/components/CourseTopics/CourseTopics.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import './CourseTopics.css';
 
+function CourseTopics() {
+  return (
+    <NavDropdown title="Course Topics" id="collasible-nav-dropdown" className="course-topics">
+      <NavDropdown.Item href="/section1">1. Introduction to Web3 and Cryptocurrencies</NavDropdown.Item>
+      {/* Add other NavDropdown.Item components for each section */}
+    </NavDropdown>
+  );
+}
+
+export default CourseTopics;
