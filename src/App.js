4463919d2c3321a032ca0006f3d2735465fb963a
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 13:48:47 +0200

    sidebar and footer location fix

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import './App.css';
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import Footer from './components/Footer/Footer';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 import Home from './components/Home/Home';
 import Section1 from './components/Sections/Section1';
 
