46e12016d13461707aee5c6926f56113d99579b9
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-03-28 11:54:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import React from 'react';
-import { Container, Dropdown } from 'react-bootstrap';
+import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Header';
+import Section1 from './Section1';
 
 function App() {
   return (
     <div className="App">
-      <Container fluid className="text-center bg-light py-5">
-        <h1 className="display-4">Learn Web3</h1>
-        <p className="lead">
-          Your ultimate guide to mastering Web3, Blockchain, and Cryptocurrencies
-        </p>
-        <img
-          src="src/logo.png"
-          alt="Web3 and Blockchain illustration"
-          className="img-fluid my-4"
-        />
-        <p>
-          In today's rapidly evolving digital world, Web3 and cryptocurrencies are revolutionizing how we interact with the internet and conduct transactions. Understanding these technologies is crucial for staying ahead of the curve and unlocking new opportunities. Our comprehensive curriculum will guide you through the fundamentals of Web3, blockchain, and cryptocurrencies, empowering you to navigate this exciting landscape with confidence.
-        </p>
-        <h2 className="mb-3">Course Topics</h2>
-        <Dropdown>
-          <Dropdown.Toggle variant="primary" id="dropdown-basic">
-            Select a Topic
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#introduction">1. Introduction to Web3 and Blockchain</Dropdown.Item>
-            <Dropdown.Item href="#crypto">2. Cryptocurrencies and Tokens</Dropdown.Item>
-            <Dropdown.Item href="#wallets">3. Wallets and Key Management</Dropdown.Item>
-            <Dropdown.Item href="#smart-contracts">4. Smart Contracts and DApps</Dropdown.Item>
-            <Dropdown.Item href="#decentralized-storage">5. Decentralized Storage and Data Privacy</Dropdown.Item>
-            <Dropdown.Item href="#scaling">6. Scaling and Interoperability</Dropdown.Item>
-            <Dropdown.Item href="#nfts">7. Non-Fungible Tokens (NFTs)</Dropdown.Item>
-            <Dropdown.Item href="#defi">8. Decentralized Finance (DeFi)</Dropdown.Item>
-            <Dropdown.Item href="#dao">9. Decentralized Autonomous Organizations (DAOs)</Dropdown.Item>
-            <Dropdown.Item href="#web3-development">10. Web3 Development</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Learn Web3</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="Course Topics" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#section1">1. Introduction to Web3 and Cryptocurrencies</NavDropdown.Item>
+                <NavDropdown.Item href="#section2">2. Blockchain Technology Basics</NavDropdown.Item>
+                <NavDropdown.Item href="#section3">3. Ethereum and Smart Contracts</NavDropdown.Item>
+                <NavDropdown.Item href="#section4">4. Decentralized Finance (DeFi)</NavDropdown.Item>
+                <NavDropdown.Item href="#section5">5. Non-fungible Tokens (NFTs)</NavDropdown.Item>
+                <NavDropdown.Item href="#section6">6. Building Decentralized Applications (dApps)</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Header />
+      <Section1 />
     </div>
   );
 }
