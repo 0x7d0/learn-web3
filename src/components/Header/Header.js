f200ae2f8a2145fd8dbb341fcdfae5ad3c64764a
saryd <30491318+0x7d0@users.noreply.github.com>
2023-03-29 20:35:32 +0000

    changed css for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" className="header-navbar">
-        <Navbar.Brand href="/">Learn Web3</Navbar.Brand>
-      </Navbar>
+    <header className="header">
+      <Container>
+        <div>
+          <h1 className="header-title">Learn Web3 & Cryptocurrencies</h1>
+          <h2 className="header-subtitle">Unlock the Future of Decentralized Technology</h2>
+        </div>
+      </Container>
     </header>
   );
 };
